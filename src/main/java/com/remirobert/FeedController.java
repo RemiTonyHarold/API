dfd94aa869bbf90bb2bb723c2f1bd08523fcfc03
rémi  <remirobert33530@gmail.com>
2017-01-21 11:06:48 +0100

    update root create custom source

--- src/main/java/com/remirobert/FeedController.java
+++ src/main/java/com/remirobert/FeedController.java
@@ -24,40 +24,10 @@ public class FeedController {
     @Autowired
     private FeedRepository feedRepository;
 
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private ScheduledTasks scheduledTasks;
-
     @RequestMapping(value = "/feedSource/{category}", method = RequestMethod.GET)
     public List<FeedSource> getFeedSource(HttpServletRequest request,
                                           @PathVariable("category") String category) {
         User user = (User)request.getAttribute("user");
         return feedRepository.findByCategoryIdAndOwnerId(category, user != null ? user.getId() : null);
-    }
-
-    @AuthorizationRequired
-    @RequestMapping(value = "/feedSource/{category}", method = RequestMethod.POST)
-    public FeedSource postSource(HttpServletRequest request,
-                                 @PathVariable("category") String categoryId,
-                                 @RequestParam(value = "url", required = false) String urlSource) {
-        Category category = categoryRepository.findById(categoryId);
-        if (category == null) {
-            throw new CategoryNotFoundException();
-        }
-        FeedSource feedSource = feedRepository.findByCategoryIdAndUrl(categoryId, urlSource);
-        if (feedSource != null) {
-            return feedSource;
-        }
-        User user = (User)request.getAttribute("user");
-
-        FeedSource newSource = new FeedSource();
-        newSource.setUrl(urlSource);
-        newSource.setOwnerId(user.getId());
-        newSource.setCategoryId(categoryId);
-        feedRepository.save(newSource);
-        newSource = scheduledTasks.fetchNewsFromSource(newSource);
-        return newSource;
     }
 }
--- src/main/java/com/remirobert/UserFeedController.java
+++ src/main/java/com/remirobert/UserFeedController.java
@@ -14,10 +14,16 @@ import java.util.List;
 public class UserFeedController {
 
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private FeedRepository feedRepository;
 
     @Autowired
     private NewsRepository newsRepository;
+
+    @Autowired
+    private ScheduledTasks scheduledTasks;
 
     @AuthorizationRequired
     @RequestMapping(value = "/userFeedSource", method = RequestMethod.GET)
@@ -40,5 +46,29 @@ public class UserFeedController {
         List<News> newsList = newsRepository.findBySourceId(sourceId);
         newsRepository.delete(newsList);
         feedRepository.delete(source);
+    }
+
+    @AuthorizationRequired
+    @RequestMapping(value = "/userFeedSource/{category}", method = RequestMethod.POST)
+    public FeedSource postSource(HttpServletRequest request,
+                                 @PathVariable("category") String categoryId,
+                                 @RequestParam(value = "url", required = false) String urlSource) {
+        Category category = categoryRepository.findById(categoryId);
+        if (category == null) {
+            throw new CategoryNotFoundException();
+        }
+        FeedSource feedSource = feedRepository.findByCategoryIdAndUrl(categoryId, urlSource);
+        if (feedSource != null) {
+            return feedSource;
+        }
+        User user = (User)request.getAttribute("user");
+
+        FeedSource newSource = new FeedSource();
+        newSource.setUrl(urlSource);
+        newSource.setOwnerId(user.getId());
+        newSource.setCategoryId(categoryId);
+        feedRepository.save(newSource);
+        newSource = scheduledTasks.fetchNewsFromSource(newSource);
+        return newSource;
     }
 }
